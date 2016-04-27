bb08202a843ca2121acc8c8a425d5652b1fcf6fe
Tyler Rozboril <ty.rozboril@gmail.com>
2016-04-27 10:44:16 -0600

    services and controllers

--- edU/www/js/services.js
+++ edU/www/js/services.js
@@ -1,6 +1,6 @@
 angular.module('eduApp.services', [])
 
-.service('studentService', function($http) {
+.service('studentService', function ($http) {
   return {
     getStudents: function () {
       return $http.get('https://educatedevelopunderstand.herokuapp.com/students');
@@ -10,4 +10,39 @@ angular.module('eduApp.services', [])
     }
   };
 
-});
+})
+
+.service('authService', function ($http, $window) {
+  var user = {};
+    return {
+      login: function(user) {
+        return $http.post('/auth/login', user);
+      },
+      logout: function(user) {
+        user = null;
+        $window.localStorage.clear();
+      },
+      setUserInfo: function(userData) {
+        $window.localStorage.setItem('user', JSON.stringify(userData.data.data.user));
+        $window.localStorage.setItem('token', JSON.stringify(userData.data.data.token));
+      },
+      getUserInfo: function(userData) {
+        return $window.localStorage.getItem('user');
+      },
+    };
+})
+
+.service('authInterceptor', function ($window) {
+  return {
+      request: function(config) {
+        // check for token in headers
+        // config.headers['X-requested-with'] = XMLHttpRequest;
+        var token = $window.localStorage.getItem('token');
+        if(token) {
+          config.headers.Authorization = "Bearer " + token;
+          // return $q.resolve(config);
+        }
+        return config;
+      }
+    };
+  });
